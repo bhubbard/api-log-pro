cdfa4a59b0b2e7ac28305f30801f2b6ad53c7c1a
Brandon Hubbard <bkhubbard@gmail.com>
2019-01-20 11:14:38 -0800

    more updates

--- class-api-log-pro.php
+++ class-api-log-pro.php
@@ -40,29 +40,52 @@ if( ! class_exists( 'API_Log_Pro' )) {
 		
 		public function log_requests( $response, $handler, $request ) {
 			
+			if( ! is_wp_error( $response ) ) {
+			
 			$request_uri = esc_url( $_SERVER['REQUEST_URI'] ) ?? null;
 			
-			$path = $request->get_route();
-			$method   = $request->get_method();
-			$request_headers = $request->get_headers();
-			$response_headers = $response->get_headers();
+			$path = $request->get_route() ?? '';
+			$method   = $request->get_method() ?? '';
+			$request_headers = $request->get_headers() ?? '';
+			$response_headers = $response->get_headers() ?? '';
+			
 			
 			$args = array(
 				'path' => $path ?? '',
 				'response' => $response ?? '',
 				'response_headers' => $response_headers ?? '',
 				'request_headers' => $request_headers ?? '',
-				'load_time' => $load_time ?? '',
+				'load_time' => json_encode( array( 'time' => time() ) ) ?? '',
 				'requested_at' => current_time( 'mysql' ) ?? '0000-00-00 00:00:00',
 			); 
 			
-			$this->add_api_log( $args );
+			$inserted_log_id = $this->add_api_log( $args );
 			
+			$query_count 		= get_num_queries() ?? '';
+			$memory_usage 		= round( size_format( memory_get_usage() ), 2 ) ?? '';
+			$memory_peak_usage 	= round( size_format( memory_get_peak_usage() ), 2 ) ?? '';
+			$memory_limit 		= round( size_format( $this->let_to_num( WP_MEMORY_LIMIT ) ), 2 ) ?? '';
+			
+			
+			$this->add_log_meta( $inserted_log_id, 'query_count', $query_count, true );
+			$this->add_log_meta( $inserted_log_id, 'memory_usage', $memory_usage, true );
+			$this->add_log_meta( $inserted_log_id, 'memory_peak_usage', $memory_peak_usage, true );
+			$this->add_log_meta( $inserted_log_id, 'memory_limit', $memory_limit, true );
+			
+			}
+			
+			// Return Response.
 			return $response;
 			
 		}
 
-
+		
+		/**
+		 * Add API Log.
+		 * 
+		 * @access public
+		 * @param array $args (default: array()) Arguments.
+		 */
 		public function add_api_log( $args = array() ) {
 			
 			global $wpdb;
@@ -73,10 +96,9 @@ if( ! class_exists( 'API_Log_Pro' )) {
 			$response      			= $args['response'] ?? '';
 			$response_headers       = $args['response_headers'] ?? '';
 			$request_headers 		= $args['request_headers'] ?? '';
-			$load_time 				= $args['request_headers'] ?? '';
+			$load_time 				= $args['load_time'] ?? '';
 			$requested_at   		= $args['requested_at'] ?? '';
-		
-			$wpdb->show_errors();
+	
 			
 			$results = $wpdb->insert(
 				$table,
@@ -86,15 +108,41 @@ if( ! class_exists( 'API_Log_Pro' )) {
 					'response'				 => wp_json_encode( $response ),
 					'response_headers'       => wp_json_encode( $response_headers ),
 					'request_headers'        => wp_json_encode( $request_headers ),
-					'load_time'				 => '5',
+					'load_time'				 => $load_time,
 					'requested_at'   		 => $requested_at ?? '0000-00-00 00:00:00',
 				),
 				array( '%d', '%s', '%s', '%s', '%s', '%d', '%s' )
 			);
 		
-			 return $results;
+			return $wpdb->insert_id;
 			
 			
+		}
+		
+		/**
+		 * Let to Number.
+		 *
+		 * This function transforms the php.ini notation for numbers (like '2M') to an integer.
+		 *
+		 * @param $size Size.
+		 * @return int Int.
+		 */
+		public function let_to_num( $size ) {
+		    $l 		 = substr( $size, -1 );
+		    $ret 	 = substr( $size, 0, -1 );
+		    switch( strtoupper( $l ) ) {
+			    case 'P':
+			        $ret *= 1024;
+			    case 'T':
+			        $ret *= 1024;
+			    case 'G':
+			        $ret *= 1024;
+			    case 'M':
+			        $ret *= 1024;
+			    case 'K':
+			        $ret *= 1024;
+		    }
+		    return $ret;
 		}
 
 		
@@ -102,9 +150,9 @@ if( ! class_exists( 'API_Log_Pro' )) {
 		 * Get Log Meta.
 		 * 
 		 * @access public
-		 * @param mixed $log_id
-		 * @param string $key (default: '')
-		 * @param bool $single (default: false)
+		 * @param mixed $log_id Log ID.
+		 * @param string $key (default: '') Key.
+		 * @param bool $single (default: false) Single.
 		 */
 		public function get_log_meta( $log_id, string $key = '', bool $single = false  ) {
 				
@@ -117,10 +165,10 @@ if( ! class_exists( 'API_Log_Pro' )) {
 		 * Add Log Meta..
 		 * 
 		 * @access public
-		 * @param mixed $log_id
-		 * @param string $key
-		 * @param mixed $value
-		 * @param bool $unique (default: false)
+		 * @param mixed $log_id Log ID.
+		 * @param string $key Key.
+		 * @param mixed $value Value.
+		 * @param bool $unique (default: false) Unique.
 		 */
 		public function add_log_meta( $log_id, string $key, $value, $unique = false ) {
 			
@@ -133,10 +181,10 @@ if( ! class_exists( 'API_Log_Pro' )) {
 		 * Update Log Meta.
 		 * 
 		 * @access public
-		 * @param mixed $log_id
-		 * @param string $key
-		 * @param mixed $value
-		 * @param string $prev_value (default: '')
+		 * @param mixed $log_id Log ID.
+		 * @param string $key Meta Key.
+		 * @param mixed $value Meta Value.
+		 * @param string $prev_value (default: '') Previous Value.
 		 */
 		public function update_log_meta( $log_id, string $key, $value, $prev_value = '' ) {
 			
@@ -149,10 +197,10 @@ if( ! class_exists( 'API_Log_Pro' )) {
 		 * Delete Log Meta.
 		 * 
 		 * @access public
-		 * @param mixed $log_id
-		 * @param string $key (default: '')
-		 * @param string $value (default: '')
-		 * @param bool $delete_all (default: false)
+		 * @param mixed $log_id Log ID.
+		 * @param string $key (default: '') Key.
+		 * @param string $value (default: '') Value.
+		 * @param bool $delete_all (default: false) Delete All.
 		 */
 		public function delete_log_meta( $log_id, string $key = '', string $value = '', bool $delete_all = false ) {
 			
