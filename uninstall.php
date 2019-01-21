498882ec4eadacd09abf8f68fc1770416f26c982
Brandon Hubbard <bkhubbard@gmail.com>
2019-01-20 20:46:18 -0800

    cleanup

--- uninstall.php
+++ uninstall.php
@@ -12,10 +12,10 @@ defined( 'ABSPATH' ) || exit;
 	global $wpdb;
 
 	// Drop Log Table.
-	$log_table   = $wpdb->prefix . 'api_log_pro';
-	$results = $wpdb->query( $wpdb->prepare( 'DROP TABLE IF EXISTS %1s', $log_table ) );
+	$log_table = $wpdb->prefix . 'api_log_pro';
+	$results   = $wpdb->query( $wpdb->prepare( 'DROP TABLE IF EXISTS %1s', $log_table ) );
 
 	// Drop Meta Table.
-	$meta_table   = $wpdb->prefix . 'api_log_pro_meta';
-	$results = $wpdb->query( $wpdb->prepare( 'DROP TABLE IF EXISTS %1s', $meta_table ) );
+	$meta_table = $wpdb->prefix . 'api_log_pro_meta';
+	$results    = $wpdb->query( $wpdb->prepare( 'DROP TABLE IF EXISTS %1s', $meta_table ) );
 
